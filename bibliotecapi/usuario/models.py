3a41373a5cdd94e52e5208d45a69ace22f215e8a
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-02-10 10:48:43 -0300

    Implementação do retorno string e autopreenchimento de id através de uuid na classe usuario

--- bibliotecapi/usuario/models.py
+++ bibliotecapi/usuario/models.py
@@ -1,10 +1,15 @@
+import uuid
 from django.db import models
+
 
 # Create your models here.
 
 class Usuario(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     cpf = models.PositiveBigIntegerField(unique=True, null=False)
     nome = models.TextField()
     data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
 
