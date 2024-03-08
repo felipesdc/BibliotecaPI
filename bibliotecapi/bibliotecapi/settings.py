2713c45908136b341372d3e34a56de35d90ef9bf
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-03-07 22:22:18 -0300

    Alteração na abordagem, modelo baseado no MDN

--- bibliotecapi/bibliotecapi/settings.py
+++ bibliotecapi/bibliotecapi/settings.py
@@ -31,9 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'emprestimo.apps.EmprestimoConfig',
-    'usuario.apps.UsuarioConfig',
-    'livro.apps.LivroConfig',
+    'acervo.apps.AcervoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- bibliotecapi/livro/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("cadastrar/", views.cadastrar, name="cadastrar")
-]
