2d50d5e59d79fc6ba523d876ba7139e56626304c
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-04-02 22:07:44 -0300

    inclusão da visualização listagem e detalhe para os modelos do acervo

--- bibliotecapi/acervo/urls.py
+++ bibliotecapi/acervo/urls.py
@@ -4,4 +4,31 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('livros/', views.ListaLivrosView.as_view(), name='livros'),
+    path('livro/<int:pk>', views.DetalheLivroView.as_view(), name='detalhe-livro'),
+
+]
+
+urlpatterns += [
+    path('autores/', views.AutorListView.as_view(), name='autores'),
+    path('autor/<int:pk>',
+         views.AutorDetailView.as_view(), name='detalhe-autor'),
+]
+
+urlpatterns += [
+    path('linguas/', views.LinguaListView.as_view(), name='linguas'),
+    path('lingua/<int:pk>',
+         views.LinguaDetailView.as_view(), name='detalhe-lingua'),
+]
+
+urlpatterns += [
+    path('generos/', views.GeneroListView.as_view(), name='generos'),
+    path('genero/<int:pk>',
+         views.GeneroDetailView.as_view(), name='detalhe-genero'),
+]
+
+urlpatterns += [
+    path('copias/', views.LivroCopiaListView.as_view(), name='copias'),
+    path('copia/<uuid:pk>', views.LivroCopiaDetailView.as_view(),
+         name='detalhe-copia'),
 ]
--- bibliotecapi/acervo/views.py
+++ bibliotecapi/acervo/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
-# Create your views here.
-from .models import Livro, Autor, LivroCopia, Genero
+from .models import Livro, Autor, LivroCopia, Genero, Lingua
+
 
 def index(request):
-    """View function for home page of site."""
+    """Função para criação de visualização inicial da página."""
 
-    # Generate counts of some of the main objects
+    # Gerando contagens para alguns itens do acervo
     numero_de_livros = Livro.objects.all().count()
     numero_de_copias = LivroCopia.objects.all().count()
+    numero_de_generos = Genero.objects.all().count()
 
-    # Available books (status = 'a')
+    # Livros Disponíveis para Empréstimo (estado = 'd')
     numero_de_copias_disponiveis = LivroCopia.objects.filter(estado__exact='d').count()
 
-    # The 'all()' is implied by default.
+    # O 'all()' é implícito por padrão
     numero_de_autores = Autor.objects.count()
 
     context = {
@@ -21,7 +23,75 @@ def index(request):
         'numero_de_copias': numero_de_copias,
         'numero_de_copias_disponiveis': numero_de_copias_disponiveis,
         'numero_de_autores': numero_de_autores,
+        'numero_de_generos': numero_de_generos
     }
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+
+class ListaLivrosView(generic.ListView):
+    model = Livro
+    context_object_name = 'lista_livros'
+    template_name = 'acervo/lista_livros.html'
+    paginate_by = 10
+
+class DetalheLivroView(generic.DetailView):
+    model = Livro
+    context_object_name = 'livro'
+    template_name = 'acervo/detalhe_livro.html'
+
+class AutorListView(generic.ListView):
+    """Classe genérica para visão lista de Autores."""
+    model = Autor
+    context_object_name = 'lista_autores'
+    template_name = 'acervo/lista_autores.html'
+    paginate_by = 10
+
+class AutorDetailView(generic.DetailView):
+    """Classe genérica para visão detalhe do Autor."""
+    model = Autor
+    context_object_name = 'autor'
+    template_name = 'acervo/detalhe_autor.html'
+
+class GeneroListView(generic.ListView):
+    """Classe genérica para visão detalhe do Gênero."""
+    model = Genero
+    context_object_name = 'lista_generos'
+    template_name = 'acervo/lista_generos.html'
+    paginate_by = 10
+
+
+class GeneroDetailView(generic.DetailView):
+    """Classe genérica para visão lista de Gêneros."""
+    model = Genero
+    context_object_name = 'genero'
+    template_name = 'acervo/detalhe_genero.html'
+
+class LinguaListView(generic.ListView):
+    """Classe genérica para visão detalhe da língua."""
+    model = Lingua
+    context_object_name = 'lista_linguas'
+    template_name = 'acervo/lista_linguas.html'
+    paginate_by = 10
+
+class LinguaDetailView(generic.DetailView):
+    """Classe genérica para visão lista das Línguas."""
+    model = Lingua
+    context_object_name = 'lingua'
+    template_name = 'acervo/detalhe_lingua.html'
+
+class LivroCopiaListView(generic.ListView):
+    """Classe genérica para visão lista das Cópias de Livros."""
+    model = LivroCopia
+    context_object_name = 'lista_copias'
+    template_name = 'acervo/lista_copias.html'
+    paginate_by = 10
+
+
+class LivroCopiaDetailView(generic.DetailView):
+    """Classe genérica para visão detalhe da Cópia de Livro."""
+    model = LivroCopia
+    context_object_name = 'copia'
+    template_name = 'acervo/detalhe_copia.html'
