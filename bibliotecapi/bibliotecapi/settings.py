378565d7a64d3978083fb5c51f41dfcd80459bed
Felipe Santiago de Carvalho <50447612+felipesdc@users.noreply.github.com>
2024-04-20 23:42:16 -0300

    ajuste requirements, production e whitenoise

--- bibliotecapi/bibliotecapi/settings.py
+++ bibliotecapi/bibliotecapi/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'acervo.apps.AcervoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
