f91d35bd289cc62e09b53bea5a1bebb4725adb90
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-07-07 20:55:00 -0300

    acerto de arquivos estaticos locais

--- bibliotecapi/bibliotecapi/settings.py
+++ bibliotecapi/bibliotecapi/settings.py
@@ -130,8 +130,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
