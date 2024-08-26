20f5a1437a486fbdce2927d2c2138a0ef3dc2e5f
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-08-26 19:25:27 -0300

    update Pipfile

--- bibliotecapi/build_files.sh
+++ bibliotecapi/build_files.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+python3.12 --version
 # Update pip
 echo "Updating pip..."
-pip install -U pip
+python3.12 -m pip install -U pip
 
 # Install dependencies
 
 echo "Installing project dependencies..."
-pip install -r requirements.txt
+python3.12 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
 
 # Collect staticfiles
 echo "Collect static..."
-python manage.py collectstatic --noinput --clear
+python3.12 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
