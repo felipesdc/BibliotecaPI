73dfc2a7dc6c90a9db5fb594a6446192559eedc1
Felipe Santiago de Carvalho <felipesdc@gmail.com>
2024-08-26 15:54:11 -0300

    acerto versão interpretador para 3.12

--- bibliotecapi/build_files.sh
+++ bibliotecapi/build_files.sh
@@ -2,20 +2,20 @@
 
 # Update pip
 echo "Updating pip..."
-python3.9 pip install -U pip
+python3 pip install -U pip
 
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 # Collect staticfiles
 echo "Collect static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
